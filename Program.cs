51029b1e391a8620c92a29632e54a70a5600fcc5
Sébastien Santoro <dereckson@espace-win.org>
2013-11-24 16:20:57 +0100

    Initial version

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ListExtensions {
+    /// <summary>
+    /// The main program
+    /// </summary>
+    class Program {
+        /// <summary>
+        /// The main point of entry of the application
+        /// </summary>
+        /// <param name="args">The application arguments.</param>
+        static void Main (string[] args) {
+            #region Parse options
+            string directory;
+
+            if (args.Length == 0) {
+                directory = Environment.CurrentDirectory;
+            } else {
+                directory = args[0];
+                if (!Directory.Exists(directory)) {
+                    Console.Error.WriteLine("Directory not found: {0}", directory);
+                    return;
+                }
+            }
+            #endregion
+
+            #region Procedural code
+            string[] extensions = GetExtensionsInDirectory(directory, true);
+            Console.Write(String.Join(" ", extensions));
+            #endregion
+        }
+
+        /// <summary>
+        /// Gets the extensions in directory.
+        /// </summary>
+        /// <param name="directory">The directory.</param>
+        /// <param name="lookInSubDirectories">if set to <c>true</c>, looks in subdirectories.</param>
+        /// <returns>
+        /// An array containing the extensions of files found in directory.
+        /// </returns>
+        public static string[] GetExtensionsInDirectory (string directory, bool lookInSubDirectories) {
+            HashSet<string> extensions = new HashSet<string>();
+            DirectoryInfo di = new DirectoryInfo(directory);
+            
+            //Look the files in the current directories, and get an unique list of extensions
+            foreach (FileInfo fi in di.GetFiles()) {
+                extensions.Add(fi.Extension);
+            }
+
+            //In recursive, looks also in subdirectories
+            if (lookInSubDirectories) {
+                foreach (DirectoryInfo subDi in di.GetDirectories()) {
+                    extensions.UnionWith(GetExtensionsInDirectory(subDi.FullName, true));
+                }
+            }
+
+            string[] result = new string[extensions.Count];
+            extensions.CopyTo(result);
+            return result;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ListExtensions")]
+[assembly: AssemblyDescription("Lists all the extension in the current or specified folder")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Espace Win Open Source Project")]
+[assembly: AssemblyProduct("ListExtensions")]
+[assembly: AssemblyCopyright("Released under BSD license. Espace Win Open Source Project, 2013.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fad095ee-2c67-4294-ac48-24d6c9a736fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
